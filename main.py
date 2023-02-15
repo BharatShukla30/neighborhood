bf7b45ec255ee4d81d6a96b30bd23bc53e059991
Bharat Shukla <bharatsagii30@gmail.com>
2023-02-15 23:06:43 +0530

    added geolocation based finding of nearest donors or orgs

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import sqlite3
-import re
 
+from math import radians, sin, cos, sqrt, atan2
 from constants import ARCGIS_URL, ARCGIS_API_KEY
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 from arcgis.gis import GIS
-from arcgis.geocoding import geocode, reverse_geocode
+from arcgis.geocoding import geocode
 
-gis = GIS(ARCGIS_URL, api_key = ARCGIS_API_KEY)
+gis = GIS(ARCGIS_URL, api_key=ARCGIS_API_KEY)
 app = Flask(__name__)
 
 def init_db():
@@ -46,7 +46,7 @@ def register():
         email = request.form['email']
         blood_group = request.form['blood_group']
         user_type = request.form['user_type']
-        location = request.form['location'] 
+        location = request.form['location']
         location = location_formatting(location)
         
         conn = sqlite3.connect('users.db')
@@ -61,7 +61,50 @@ def register():
         return 'User registered successfully!'
 
     return render_template('form.html')
-    
+
+
+@app.route('/nearest_users/<int:user_id>/<int:d>', methods=['GET'])
+def get_nearest_users(user_id, d):
+    conn = sqlite3.connect('users.db')
+    c = conn.cursor()
+
+    c.execute("SELECT * FROM users WHERE id = ?", (user_id,))
+    user = c.fetchone()
+    if user is None:
+        return jsonify({'error': 'User not found'})
+
+    nearest_users = []
+    for row in c.execute("SELECT * FROM users WHERE id != ?", (user_id,)):
+        latitude, longitude = row[5].split(', ')
+        distance = calc_distance(float(latitude), float(longitude), float(user[5].split(', ')[0]), float(user[5].split(', ')[1]))
+        if distance <= d:
+            nearest_users.append({'id': row[0], 'name': row[1], 'distance': distance})
+
+    nearest_users = sorted(nearest_users, key=lambda x: x['distance'])
+    conn.close()
+    return jsonify(nearest_users)
+
+
+def calc_distance(lat1, lon1, lat2, lon2):
+    """
+    Calculation of geo-distance using the haversine formula
+    """
+    R = 6373.0  # approximate radius of Earth in km
+
+    lat1_rad = radians(lat1)
+    lon1_rad = radians(lon1)
+    lat2_rad = radians(lat2)
+    lon2_rad = radians(lon2)
+
+    dlon = lon2_rad - lon1_rad
+    dlat = lat2_rad - lat1_rad
+
+    a = sin(dlat / 2) ** 2 + cos(lat1_rad) * cos(lat2_rad) * sin(dlon / 2) ** 2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+    distance = R * c
+    return distance
+
 if __name__ == '__main__':
     init_db()
     app.run(debug=True)
---
+++ model/Users.py
@@ -0,0 +1,15 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    email = db.Column(db.String(120), unique=True)
+    blood_group = db.Column(db.String(3))
+    location = db.Column(db.String(100))
+    user_type = db.Column(db.String(100))
+    query_class = db.Query
+    def __repr__(self):
+        return '<User %r>' % self.name
+
