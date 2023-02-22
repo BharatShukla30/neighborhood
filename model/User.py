6a117c5040948ead3856e075e8f5604d4916c12b
Bharat Shukla <bharatsagii30@gmail.com>
2023-02-22 17:58:38 +0530

    missed pwd field in USER model

--- model/User.py
+++ model/User.py
@@ -6,6 +6,7 @@ class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
     email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
     blood_group = db.Column(db.String(3))
     location = db.Column(db.String(100))
     user_type = db.Column(db.String(100))
