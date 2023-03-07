532531de09e2fb307ee86d47fb4c4b6bfd7ceae1
Parmesh Kumar <parmeshkumar414@gmail.com>
2023-03-07 11:20:43 +0530

    Location model Added

--- model/Location.py
+++ model/Location.py
@@ -3,7 +3,7 @@ from User import Users
 db = SQLAlchemy()
 
 class Location(db.Model):
-    id = db.Column(db.Integer, db.ForeignKey('User.id'))
+    id = db.Column(db.Integer, db.ForeignKey('Users.id'))
     location = db.Column(db.String(100))
     city = db.Column(db.String(100))
     state = db.Column(db.String(100))
